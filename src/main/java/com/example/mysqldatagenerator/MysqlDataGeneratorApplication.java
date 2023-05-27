3871ee5ba9fee0481f698670b557a8c86c9f32da
anderwang <ander.wang@geminidata.com>
2023-05-27 16:27:50 +0800

    init

---
+++ src/main/java/com/example/mysqldatagenerator/MysqlDataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqldatagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlDataGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlDataGeneratorApplication.class, args);
+	}
+
+}
